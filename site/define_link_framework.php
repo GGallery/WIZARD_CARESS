02a7a8d68df9b68bc9d0a974fc291a8e365bfd6b
Tony <antonio@ggallery.it>
2017-12-13 14:16:02 +0100

    Aggiunta pagina link esterni su framework

--- site/define_link_framework.php
+++ site/define_link_framework.php
@@ -18,5 +18,3 @@ define('MANAGE_SET_COURSE_LEARNING_OUTCOMES_ENTRY','manage-set-course-learning-o
 define('MANAGE_COURSE_LEARNING_OUTCOMES_ENTRY','manage-course-learning-outcomes-entry');
 define('MANAGE_HHCP_IN_COUNTRY_ENTRY','manage-hhcp-in-country');
 define('MANAGE_ROLE_ELEMENTS_ENTRY','manage-role-elements');
-
-?>
