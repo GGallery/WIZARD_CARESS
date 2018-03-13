632453910af61320adef6705d22386f8c8830bca
Tony <[tony@bslt.it]>
2018-03-13 17:50:14 +0100

    background

--- site/views/case2/tmpl/default_g2c.php
+++ site/views/case2/tmpl/default_g2c.php
@@ -26,7 +26,7 @@
                     <input type="hidden"  name="option" value="com_cck">
                     <input type="hidden"  name="view" value="form">
                     <input type="hidden"  name="layout" value="edit">
-                    <input type="hidden"  name="type" value="role_element_list">
+                    <input type="hidden"  name="type" value="role_element_role">
                     <button type="submit" class="btn" >click here</button>
                 </form>
 
