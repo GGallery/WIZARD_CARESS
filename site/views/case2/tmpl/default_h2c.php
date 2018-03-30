10d0945caa0bdc18ff59f03fcce12973df60fdac
Tony <[tony@bslt.it]>
2018-03-30 18:19:00 +0200

    fix

--- site/views/case2/tmpl/default_h2c.php
+++ site/views/case2/tmpl/default_h2c.php
@@ -68,7 +68,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Esco classification list</h4>
+                <h4 class="modal-title">Learning Unit list</h4>
             </div>
             <div class="modal-body">
                 <ul>
