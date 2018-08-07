3ded587149fe91125ef846ae5a56cbb255998819
Tony <[tony@bslt.it]>
2018-08-07 19:29:37 +0200

    s1c init

---
+++ site/views/case3/tmpl/language/en/s1c.php
@@ -0,0 +1,60 @@
+<div class="c3container">
+    <h2>ACTIVITY 1: COMPETENCY GAP DEFINITION</h2>
+
+
+    <span class="avanzamento">
+        <span class="lightarrow">
+            <br>
+            STEP 1: COLLECT INFORMATION
+        </span>
+
+        <span class="darkarrow">
+            <br>
+            STEP2:  DEFINE A COMPETENCY LIST
+        </span>
+
+        <span class="lightarrow">
+            STEP 3: IDENTIFY THE COMPETENCY GAP
+        </span>
+    </span>
+
+    <p>
+        After collecting the needed information, <strong> making a list of possible competencies identifying the “competency gap”</strong>.
+    </p>
+
+    <h5>What is a Competency?</h5>
+
+    <p>Conventionally in the CARESS project the word competency is used to describe the  capability to apply or use a set of related knowledge and skills (with a certain level of responsibility and autonomy) needed in a defined work setting  to perform “critical work functions” or tasks in a successful way; this feature “includes both visible competencies of knowledge and skills and underlying elements of competencies, like traits and motives”.</p>
+
+    <span class="focus">
+        THINK ABOUT “KEY ACTIVITIES “ which should be performed by the professional you want to target AND LIST THEM
+    </span>
+
+    <h5>What are Key Activities?</h5>
+    <p>
+        <strong>Key Activities are an integrated group of competencies</strong>, which are in their entirety necessary to perform a task relevant to a job profile. The key activities of one profession must altogether cover all activities for the performance of a profession, regardless of its application context.
+        <br>
+        <em>Examples of a Key Activity could be “to provide therapeutic education to the users and their caregivers” or “to monitor frailty and health conditions”</em>
+    </p>
+
+    <span class="focus">
+        DEFINE COMPETENCIES which should characterize each “key activity” AND LIST THEM
+    </span>
+
+    <p>Competencies can be defined as the “knowledge” and “skills” needed to perform the above key activities together with the level of responsibility and autonomy which the professional is expected to adopt in the key activity.
+        In addition, you can distinguish between:
+    </p>
+
+    <ul class="listapallino">
+        <li>
+            <em>Competences that are horizontal to different professionals involved in homecare</em> (transversal competence gap), such as knowledge of the rules and laws regarding homecare or practical skills about the use of health monitoring ICTs, etc.
+        </li>
+        <li>
+            <em>Competences that are specific of the targeted professional</em>, such as practical skills about therapeutic exercises targeting older adults for physiotherapists, or the knowledge of the local network of social services for homecare assistants.
+        </li>
+    </ul>
+
+    <a href="components/com_wizard/files/example1.pdf" target="_blank" class="exampleLink">Click on the icon to see an EXAMPLE of a possible LIST.</a>
+
+
+</div>
