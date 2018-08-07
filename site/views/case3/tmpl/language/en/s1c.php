a479467a4d0f1b23cc6b80da49f46dca28bb78ce
Tony <[tony@bslt.it]>
2018-08-07 19:33:59 +0200

    s1c fine

--- site/views/case3/tmpl/language/en/s1c.php
+++ site/views/case3/tmpl/language/en/s1c.php
@@ -1,60 +1,38 @@
 <div class="c3container">
     <h2>ACTIVITY 1: COMPETENCY GAP DEFINITION</h2>
 
-
     <span class="avanzamento">
         <span class="lightarrow">
             <br>
             STEP 1: COLLECT INFORMATION
         </span>
 
-        <span class="darkarrow">
+        <span class="lightarrow">
             <br>
             STEP2:  DEFINE A COMPETENCY LIST
         </span>
 
-        <span class="lightarrow">
+        <span class="darkarrow">
             STEP 3: IDENTIFY THE COMPETENCY GAP
         </span>
     </span>
 
     <p>
-        After collecting the needed information, <strong> making a list of possible competencies identifying the “competency gap”</strong>.
+        After defining the list of competencies which should characterize the profile of the homecare professional you’re targeting, you should compare this list with a number of information in order to identify a possible competency gap.
     </p>
 
-    <h5>What is a Competency?</h5>
-
-    <p>Conventionally in the CARESS project the word competency is used to describe the  capability to apply or use a set of related knowledge and skills (with a certain level of responsibility and autonomy) needed in a defined work setting  to perform “critical work functions” or tasks in a successful way; this feature “includes both visible competencies of knowledge and skills and underlying elements of competencies, like traits and motives”.</p>
-
     <span class="focus">
-        THINK ABOUT “KEY ACTIVITIES “ which should be performed by the professional you want to target AND LIST THEM
+        COMPARE YOUR COMPETENCY LIST with the information collected in STEP 1
     </span>
 
-    <h5>What are Key Activities?</h5>
     <p>
-        <strong>Key Activities are an integrated group of competencies</strong>, which are in their entirety necessary to perform a task relevant to a job profile. The key activities of one profession must altogether cover all activities for the performance of a profession, regardless of its application context.
-        <br>
-        <em>Examples of a Key Activity could be “to provide therapeutic education to the users and their caregivers” or “to monitor frailty and health conditions”</em>
+        Then you can write down a list of competencies <strong>which SHOULD characterize the professional you’re targeting but are NOT ACTUALLY included in the professional profile</strong>
     </p>
 
     <span class="focus">
-        DEFINE COMPETENCIES which should characterize each “key activity” AND LIST THEM
+        WRITE DOWN THE “COMPETENCY GAP LIST”  possibly distinguishing between "horizontal" and "profession-specific" GAP
     </span>
 
-    <p>Competencies can be defined as the “knowledge” and “skills” needed to perform the above key activities together with the level of responsibility and autonomy which the professional is expected to adopt in the key activity.
-        In addition, you can distinguish between:
-    </p>
-
-    <ul class="listapallino">
-        <li>
-            <em>Competences that are horizontal to different professionals involved in homecare</em> (transversal competence gap), such as knowledge of the rules and laws regarding homecare or practical skills about the use of health monitoring ICTs, etc.
-        </li>
-        <li>
-            <em>Competences that are specific of the targeted professional</em>, such as practical skills about therapeutic exercises targeting older adults for physiotherapists, or the knowledge of the local network of social services for homecare assistants.
-        </li>
-    </ul>
-
-    <a href="components/com_wizard/files/example1.pdf" target="_blank" class="exampleLink">Click on the icon to see an EXAMPLE of a possible LIST.</a>
-
+    <a href="components/com_wizard/files/example2.pdf" target="_blank" class="exampleLink">Click on the icon to see an EXAMPLE of a possible LIST.</a>
 
 </div>
