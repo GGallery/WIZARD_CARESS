e324e02dd8f0a2e9672901d14ed7b92f4554f37b
Tony <[tony@bslt.it]>
2018-08-07 20:03:36 +0200

    s2b finish

---
+++ site/views/case3/tmpl/language/en/s2b.php
@@ -0,0 +1,88 @@
+<div class="c3container">
+    <h2>ACTIVITY 2: LEARNING OUTCOMES IDENTIFICAT</h2>
+
+    <span class="avanzamento">
+        <span class="lightarrow">
+           STEP1: CONTEXTUAL CONSTRAINTS DEFINITION
+        </span>
+
+        <span class="darkarrow">
+            STEP2: MAP YOUR COMPETENCY LIST AGAINST THE BASELINE COURSE
+        </span>
+
+        <span class="lightarrow">
+            STEP3: DEFINE THE LEARNING OUTCOMES
+        </span>
+
+         <span class="lightarrow">
+            STEP4: GROUP THE LEARNING OUTCOMES INTO LEARNING UNITS
+        </span>
+
+    </span>
+
+    <p>
+        Now we have to start working with Learning Outcomes.
+    </p>
+
+    <h5>What are Learning Outcomes?</h5>
+
+    <p>They are statements of what a learner knows, understands and is able to do at the end of a learning process; they are defined in terms of knowledge, skills and competences.</p>
+
+    <h5>What’s the difference between Learning Outcomes and Competencies?</h5>
+
+    <p>Competencies are actually achieved learning outcomes, validated by the ability of the learner to apply autonomously his/her knowledge and skills in practice, in society and at work.</p>
+
+    <p>In order to define the Learning Outcomes of your CLM, you first have to analyze your “baseline course” with respect to the defined “competency gap”.</p>
+
+    <span class="focus">
+        FIND A DETAILED LIST OF THE LEARNING OUTCOMES OF YOUR BASELINE COURSE
+    </span>
+
+    <h5>If you need help to collect this information you can search in CARESS System https://caress.gsic.uva.es/ </h5>
+
+    <p>Learning outcomes of the baseline course should be specified in terms of knowledge, skills and transversal/personal competences.
+        <strong><u>
+                If they are not, try to list them, using the same practical suggestions provided in STEP 3.
+            </u></strong>
+    </p>
+
+    Then:
+
+    <span class="focus">
+        COMPARE YOUR “COMPETENCY GAP” with the LEARNING OUTCOMES of the BASELINE COURSE
+    </span>
+
+    This way you can identify:
+    <ul class="listapallino">
+        <li>Competencies already targeted by the course</li>
+        <li>Competencies not targeted by the course.</li>
+    </ul>
+
+    <span class="focus">
+        CREATE A LIST OF THE COMPETENCIES which are NOT TARGETED
+    </span>
+
+    <strong>They should be targeted by your CLM.</strong>
+
+    <p>Now an important issue needs to be considered.</p>
+
+    <span class="focus">
+        Define the relation between the SELECTED TARGET AND THE BASELINE COURSE:
+        "DID THE SELECTED TARGET USERS ATTEND THE BASELINE COURSE?"
+    </span>
+
+
+    <table class="table table-hover">
+        <tr >
+            <td width="10%"><strong><u>IF YES</u></strong></td>
+            <td><strong>your compensative module should focus only on the list of competences just defined in this STEP.</strong></td>
+        </tr>
+
+        <tr>
+            <td><strong><u>IF NO</u></strong></td>
+            <td><strong>you should identify which of the learning outcomes of the baseline course your specific target-users should get. Then, add these competencies to the list of competencies just defined in this STEP.</strong></td>
+        </tr>
+    </table>
+
+
+</div>
