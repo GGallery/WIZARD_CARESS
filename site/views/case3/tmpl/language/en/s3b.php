ccfdb7617ddc6199e818fe7dbe19f62fe534592d
Tony <[tony@bslt.it]>
2018-08-08 02:07:08 +0200

    s3c

---
+++ site/views/case3/tmpl/language/en/s3b.php
@@ -0,0 +1,74 @@
+<div class="c3container">
+    <h2>ACTIVITY 3: LEARNING STRATEGIES AND ASSESSMENT CRITERIA DEFINITION</h2>
+
+    <span class="avanzamento">
+        <span class="lightarrow"><br>
+           STEP 1: IDENTIFY THE "CONTRAINTS" OF YOUR DESIGN
+
+        </span>
+
+        <span class="darkarrow"><br>
+            STEP2:  DEFINE THE LEARNING STRATEGIES
+        </span>
+
+        <span class="lightarrow">
+            STEP 3: DEFINE ASSESSMENT CRITERIA, TOOLS AND ACTIVITIES
+        </span>
+
+    </span>
+
+
+    <p>
+        Take the table in which you detailed the learning outcomes of your CLM.
+    </p>
+
+    <span class="focus">
+        IDENTIFY THE PROPER LEARNING STRATEGY FOR EACH LEARNING OUTCOME
+    </span>
+
+    <p>
+        There’s no magic formula to solve design problems and there’s no effective learning strategy for every context, target user and learning outcome. <strong>But, taking into account your “design constraints”</strong> it is possible <strong>to define the most proper learning strategy for each learning outcome.</strong>
+    </p>
+
+    <p>You could formalize your choice of your learning strategies by including them in the design table you previously used to detail your learning outcomes (Activity 2 – Step3)</p>
+
+    <a href="components/com_wizard/files/annex5.pdf" class="templateLink" target="_blank">Click on the icon to DOWNLOAD a blank template to define a learning strategy for your Learning Outcomes</a>
+
+<p>
+    Each learning strategy and the relative learning activity can cover more than one learning outcome. So, after identifying the proper learning strategy for each learning outcome, try to group them according to the strategy.
+</p>
+
+    <p>So, if needed</p>
+
+    <span class="focus">
+        REVISE THE GROUPING OF LEARNING OUTCOMES INTO LEARNING UNITS, TAKING INTO ACCOUNT THE LEARNING STRATEGIES
+    </span>
+
+    <p>
+        As to the identification of the proper Learning Strategies, a number theories, well -stablished practices and patterns have formalized and shared by experts
+    </p>
+
+
+    <a href="components/com_wizard/files/annex6.pdf" class="templateLink" target="_blank">Click on the icon to DOWNLOAD a short list of hints you could take into account to define the proper learning strategy.</a>
+
+    <p>
+        If the “design constraints” allow/advise you to <strong>set up one or more e-learning modules</strong>, now itis the time to define them. Especially in adult training, learning time is particularly precious so, when contextual constraints are favorable, it is advisable to use e-learning to address specific learning objectives.
+    </p>
+
+
+    <span class="focus">
+        IDENTIFY POSSIBLE E-LEARNING MODULES
+    </span>
+
+    <p>
+        Interactive multimedia materials, videos or downloadable papers can often replace a traditional presence lesson, when it targets theoretical or factual knowledge.
+    </p>
+
+    <p>
+        Moreover e-learning can be used also to address other kind of learning objectives and to implement more active learning strategies.
+    </p>
+
+    <a href="components/com_wizard/files/example3.pdf" target="_blank" class="exampleLink">Click on the icon to see a picture representing some examples of learning strategies which can be implemented in presence or distance learning (or both of them), through collaborative or individual activities.</a>
+
+
+</div>
---
+++ site/views/case3/tmpl/language/en/s3c.php
@@ -0,0 +1,57 @@
+<div class="c3container">
+    <h2>ACTIVITY 3: LEARNING STRATEGIES AND ASSESSMENT CRITERIA DEFINITION</h2>
+
+    <span class="avanzamento">
+        <span class="lightarrow"><br>
+           STEP 1: IDENTIFY THE "CONTRAINTS" OF YOUR DESIGN
+
+        </span>
+
+        <span class="lightarrow"><br>
+            STEP2:  DEFINE THE LEARNING STRATEGIES
+        </span>
+
+        <span class="darkarrow">
+            STEP 3: DEFINE ASSESSMENT CRITERIA, TOOLS AND ACTIVITIES
+        </span>
+
+    </span>
+
+
+    <p>
+        The definition of assessment criteria is often put off to the last phases of the design process. Actually, after defining the learning outcomes and strategies of your CLM, you already have the main information necessary to define a consistent set of assessment criteria
+    </p>
+
+    <span class="focus">
+        DEFINE ASSESSMENT CRITERIA and CONDITIONS
+    </span>
+
+    <p>
+        <strong>We defined learning outcomes by using “active verbs”</strong> (Action 2 – Step 3), that helps us to identify <strong>what the learner should actually do to show he/she masters the competence</strong>. The Learning Outcome should also state the expected level of the competence  as well as <strong>the level of responsibility and autonomy of the learner</strong>.
+    </p>
+
+    <p>
+        To define the assessment CRITERIA this information should be integrated with the <strong>definition of the ASSESSEMENT CONDITIONS</strong>
+    </p>
+
+    <p>
+        Sometimes a <strong>description of the professional situation </strong>could be very useful, as well as the reference to the Key Activities analyzed at the beginning of the design process (Activity 1 – Step 2)
+        <strong>Additional conditions (requirements)</strong>, can be also defined; for instance to emulate real-life professional working conditions, a limited amount of time can be assigned daily for each activity, so that the person under assessment operates in similar stress conditions; the equipment available must be clearly defined, as well as specific products and technical support in accordance to the professional situation under evaluation.
+    </p>
+    <p>
+        Then the design of the assessment should go deeper <strong>defining specific tools </strong>(such as tests) or activities (such as problem-solving activities, demonstrations, etc.)
+    </p>
+
+    <span class="focus">
+        DEFINE ASSESSMENT TOOLS AND ACTIVITIES
+    </span>
+
+    <p> The organization of complex situated learning activities, such as an apprenticeship or work-based learning, could also allow to assess the learner in a real context with respect to a number of skills
+        and transversal competences. What is extremely important for assessment is that the designer should <strong>be aware of the competences</strong> (or their components) he/she should assess during the <strong>apprenticeship/learning
+        on the job</strong> and organize a proper setting for assessment, according to the defined criteria.</p>
+    <p> To this end, it could be important to integrate information about assessment in the design tools we’ve used so far (design tables).</p>
+
+    <a href="components/com_wizard/files/annex5.pdf" class="templateLink" target="_blank">Click on the icon to DOWNLOAD a template which you could fill in to specify the assessment criteria and conditions for your Learning Outcomes</a>
+
+
+</div>
